5fed54769038177d52b9b514eb1fdb15478dc309
Em Dash Buck <em.dash.buck@gmail.com>
2016-09-26 21:34:00 -0700

    both_ends
    0 index for string splicing, not 1 index!

--- google-python-exercises/basic/string1.py
+++ google-python-exercises/basic/string1.py
@@ -34,8 +34,10 @@ def donuts(count):
 # so 'spring' yields 'spng'. However, if the string length
 # is less than 2, return instead the empty string.
 def both_ends(s):
-  # +++your code here+++
-  return
+  if len(s) < 2 : return ''
+  f2 = s[:2]
+  l2 = s[-2:]
+  return f2+l2
 
 
 # C. fix_start
